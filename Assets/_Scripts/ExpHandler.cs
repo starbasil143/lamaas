c4c68c9549624716439428529237ea3e3756f1e5
Ayden Koyanagi <123212044+AJKoyanagi@users.noreply.github.com>
2024-12-03 01:16:42 -0600

    Particle/ XP Pickup has been implemented and tested. DeathExp is the main prefab I have been using to attach to enemies and a value = 1. Reset the transform on player to 0 for working pull.

--- Assets/_Scripts/ExpHandler.cs
+++ Assets/_Scripts/ExpHandler.cs
@@ -1,15 +1,15 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class ExpHandler: MonoBehaviour
+public class ExpHandler : MonoBehaviour
 {
     // For the Player
-    public int goldAmount = 0;
+    public int expValue = 1;
     [SerializeField] ParticleSystem exp;
     [SerializeField] Transform player;
 
     List<ParticleSystem.Particle> particles = new List<ParticleSystem.Particle>();
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -19,30 +19,35 @@ public class ExpHandler: MonoBehaviour
         }
         exp = FindFirstObjectByType<ParticleSystem>();
         exp.trigger.SetCollider(0, player);
-        
+
     }
-   
+
     private void OnParticleTrigger()
     {
         //Debug.Log("Particle has collided");
         int triggerParticles = exp.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, particles);
 
-            Debug.Log("Collided with Player");
-            for (int i = 0; i < triggerParticles; i++)
-            {
-                ParticleSystem.Particle p = particles[i];
-                goldAmount++;
-                p.remainingLifetime = 0;
-                particles[i] = p;
+        Debug.Log("Collided with Player");
+        for (int i = 0; i < triggerParticles; i++)
+        {
+            ParticleSystem.Particle p = particles[i];
+            p.remainingLifetime = 0;
+            particles[i] = p;
+            Player _player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Player>();
 
+            if (_player != null)
+            {
+                _player._exp += expValue;
+                Debug.Log("Player now has " + _player._exp + " experience pointa");
             }
             exp.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, particles);
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
